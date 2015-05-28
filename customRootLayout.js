7f2f78b8acb805828b143b329bf6f92b13ca07cc
philippspo <philipp.sporrer@planifica.de>
2015-05-28 10:54:39 +0200

    added support for full page transitions

---
+++ customRootLayout.js
@@ -0,0 +1,2 @@
+Template.customRootLayout.replaces("flowLayoutRoot");
+FlowLayout.setRoot('#momentum');
--- package.js
+++ package.js
@@ -10,12 +10,16 @@ Package.on_use(function (api, where) {
 
 	api.use(['tracker',
 			'underscore',
+			'templating',
 			'meteorhacks:flow-router@1.9.0',
-			'percolate:momentum@0.7.2'
+			'meteorhacks:flow-layout@1.3.0',
+			'percolate:momentum@0.7.2',
+			'aldeed:template-extension@3.4.3'
 		],
 		'client');
 
 	api.add_files('flow-router-transitions.js', ['client']);
+	api.add_files(['customRootLayout.html', 'customRootLayout.js'], ['client']);
 	api.add_files('style.css', ['client']);
 
 	api.export('Transitioner');
